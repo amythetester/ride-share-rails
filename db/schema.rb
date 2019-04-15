81dfb6f38a81b0e9781c757a140e6fe2956b966e
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-15 15:51:11 -0700

    created trip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_224115) do
+ActiveRecord::Schema.define(version: 2019_04_15_224950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2019_04_15_224115) do
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "phone_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.date "date"
+    t.integer "rating"
+    t.float "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
