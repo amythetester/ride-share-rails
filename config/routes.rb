e33f2745b04f34e6d576b2af60724f7dcd9d07c8
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-16 11:25:17 -0700

    added passenger, driver, and trip routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # root to: ""
+  resources :passengers
+  resources :drivers
+  resources :trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
