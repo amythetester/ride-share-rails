8d2500203e08f8b967c336ea36ccfc80d4eb1b2d
Elise Pham <elise.pham88@gmail.com>
2019-04-17 16:19:11 -0700

    added a table for trips in show passenger page, fixed routes file, get trip details to show up when clicking on the id link

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   resources :trips
 
   resources :passengers do
-    resources :trips, only: [:index, :new]
+    resources :trips, only: [:new, :show]
   end
 
   resources :drivers do
-    resources :trips, only: [:index, :new]
+    resources :trips, only: [:new, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
