2f04f21bded0a053b5b9fa5f3a593212929ceb7e
Elise Pham <elise.pham88@gmail.com>
2019-04-17 11:35:25 -0700

    added show action and passed tests for show action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,9 @@
 class TripsController < ApplicationController
+  def show
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+    if @trip.nil?
+      head :not_found
+    end
+  end
 end
--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,7 +2,28 @@ require "test_helper"
 
 describe TripsController do
   describe "show" do
-    # Your tests go here
+    it "should be OK to show an existing, valid trip" do
+      test_p = Passenger.create(name: "test passenger", phone_num: "206-xxx-xxxx")
+      test_d = Driver.create(name: "test driver", vin: "ABC3456")
+      trip = Trip.create(
+        date: "2019-03-05",
+        rating: 4, cost: 1234,
+        passenger_id: test_p.id,
+        driver_id: test_d.id,
+      )
+
+      get trip_path(trip.id)
+
+      must_respond_with :success
+    end
+
+    it "should give a 404 when giving an invalid trip id" do
+      invalid_trip_id = 10000
+
+      get trip_path(invalid_trip_id)
+
+      must_respond_with :not_found
+    end
   end
 
   describe "edit" do
