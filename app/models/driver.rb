09bbef2b978143059a110606b828bfa73dff579d
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-17 21:36:57 -0700

    added total earnings to driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -13,4 +13,13 @@ class Driver < ApplicationRecord
     avg_rating = sum / trips.count
     return avg_rating
   end
+
+  def total_earnings
+    sum = 0
+    trips = Trip.where(driver_id: self.id)
+    trips.each do |trip|
+      sum += (trip.cost - 165) * 0.8
+    end
+    return sum
+  end
 end
