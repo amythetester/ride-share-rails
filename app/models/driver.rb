cb0c0eca8ffd5150bfb87e09ec62acdaa83cb3e5
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-17 21:17:31 -0700

    added average rating to driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,14 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true, uniqueness: true
+
+  def average_rating
+    sum = 0
+    trips = Trip.where(driver_id: self.id)
+    trips.each do |trip|
+      sum += trip.rating
+    end
+    avg_rating = sum / trips.count
+    return avg_rating
+  end
 end
