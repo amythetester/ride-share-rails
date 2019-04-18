48e715ae9371c097d6a8d105f8a5d7fbf9ea306e
Elise Pham <elise.pham88@gmail.com>
2019-04-17 17:15:27 -0700

    added total_spent to passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,13 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true, uniqueness: true
+
+  def total_spent
+    sum = 0
+    trips = Trip.where(id: self.id)
+    trips.each do |trip|
+      sum += trip.cost
+    end
+    return sum
+  end
 end
